61574b128730beb9b72e84e46c126b427447f023
Rakes <niu-24-14815@niu.edu.in>
2026-06-16 16:51:17 +0530

    flaten it to a json

--- main.go
+++ main.go
@@ -12,20 +12,25 @@ func main() {
 		fmt.Println("Please provide a command")
 	}
 
+	Objective := args[1]
+
 	fmt.Println(args)
 
 	switch args[1] {
 	case "create":
+		createTodoStruct(Objective, args[0])
 		fmt.Println("creating a todo")
 	case "list":
+		createTodoStruct(Objective, args[1])
 		fmt.Println("listing all the todo")
 	case "delete": 
+		createTodoStruct(Objective, args[2])
 		fmt.Println("deleting a todo")
 	case "done":
+		createTodoStruct(Objective, args[3])
 		fmt.Println("completed a todo")
 
 	default: 
 		fmt.Println("Unknown command")
-	} 
-
+	}
 }
---
+++ todo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/google/uuid"
+)
+
+type Todos struct {
+	Id          string
+	Objective   string
+	Done        bool
+	Description string
+}
+
+func createTodoStruct(Objective string, Description string) Todos {
+	_id := uuid.New().String()
+	TodoStruct := Todos{Id: _id, Objective: Objective, Done: true, Description: Description}
+	return TodoStruct
+}
